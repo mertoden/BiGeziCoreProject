a493e766f9e619e7db0ef9637662df08e362da0c
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-07-01 18:46:15 +0300

    Servisler Yazıldı. Constructer metod eklendi.

---
+++ Business/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDL _aboutDL;
+
+        public AboutManager(IAboutDL aboutDL)
+        {
+            _aboutDL = aboutDL;
+        }
+
+        public void TAdd(About t)
+        {
+            _aboutDL.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDL.Delete(t);
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+           return _aboutDL.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDL.Update(t);
+        }
+    }
+}
