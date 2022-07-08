10501a43af1d00653a4366f10bcd50347d51ee9a
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-07-08 15:49:10 +0300

    fluentvalidaton paketleri eklendi. userlayout eklendi.

---
+++ Business/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("You cannot leave the description part blank....!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Please pick picture...!");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Please enter a 50 character description message...!");
+            RuleFor(x=>x.Description).MaximumLength(1500).WithMessage("Please shorten the description");
+        }
+    }
+}
