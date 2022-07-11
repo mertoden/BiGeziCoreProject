e63c2c18c3bdb93b68435f9b210193b2bcdebfbd
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-07-11 14:43:49 +0300

    Destination alanı yapıldı ve dinamikleştirildi. Feature kısmına chartlar eklendi.

---
+++ BiGeziCoreProject/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiGeziCoreProject.ViewComponents.Default
+{
+    public class _Statistics: ViewComponent
+    {
+         public IViewComponentResult Invoke()
+         {
+            using var c = new Context();
+            ViewBag.Rota = c.Destinations.Count();
+            ViewBag.Rehber = c.Guides.Count();
+            ViewBag.Musteri = "285";
+            return View();
+         }
+    }
+}
---
+++ Business/Concrete/FeatureManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class FeatureManager: IFeatureService
+    {
+        IFeatureDL _featureDL;
+
+
+        public FeatureManager(IFeatureDL featureDL)
+        {
+            _featureDL = featureDL;
+        }
+
+        public void TAdd(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Feature TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feature> TGetList()
+        {
+            return _featureDL.GetList();
+        }
+
+        public void TUpdate(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
