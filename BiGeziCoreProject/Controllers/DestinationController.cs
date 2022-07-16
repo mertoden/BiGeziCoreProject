1394a7c467d0caece7816cc7042bbccdfa8e73e3
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-07-16 22:35:48 +0300

    UI da düzenlemeler yapıldı. destinasyon ksımına başlandı.

--- BiGeziCoreProject/Controllers/DestinationController.cs
+++ BiGeziCoreProject/Controllers/DestinationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,21 @@ namespace BiGeziCoreProject.Controllers
 {
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDL());
         public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
         {
             return View();
         }
